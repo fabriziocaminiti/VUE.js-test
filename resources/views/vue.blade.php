a46c4b622102dd3cb45c081556d383cc150574f0
Fabrizio Caminiti <fabriziocaminiti@Air-di-Fabrizio.home-life.hub>
2020-10-10 20:47:49 +0200

    nuovo progetto in vue

---
+++ resources/views/vue.blade.php
@@ -0,0 +1,14 @@
+@extends('app-vue')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>finalmente sto cazzo di Vue</h1>
+            <p>quanto cazzo c'è voluto perchè non ho visto la chioccilina</p>
+        </div>
+    </div>
+</div>
+
+@endsection
